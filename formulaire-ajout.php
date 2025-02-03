f8dff6b5d84be1db62976a7ef01d0482b97b8cbd
sofiane <pendelliausofiane@gmail.com>
2025-02-03 11:50:03 +0100

    test

---
+++ formulaire-ajout.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="./ajout.php" method="post">
+        <h2>Ajouter un utilisateur</h2>
+        <input type="text" name="nom_user"  placeholder="Nom">
+        <input type="text" name="prenom_user"  placeholder="Prénom">
+        <input type="email" name="email_user"  placeholder="Email">
+        <input type="password" name="password_user"  placeholder="Mot de passe">
+        <input type="submit" value="Ajouter">
+    </form>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +7,25 @@
     <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-    <p></p>
+    <?php require_once("../../dbconnect.php");
+    if ($conn): ?>
+        <h1>Connexion à la BDD réussie !</h1>
+        <?php 
+        $requete = "SELECT * FROM users"; 
+        $exec = $conn->query($requete);
+        $result = $exec->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+        <hr>
+    <?php foreach($result as $key => $value): ?>
+        <p><?php echo $value["lastname"] . " " . $value["firstname"]; ?></p>
+        <ul>
+        <li><?php echo $value["email"]; ?></li>
+        <li><?php echo $value["password"]; ?></li>
+        </ul>
+        <hr>
+    <?php 
+    endforeach;
+    endif; 
+    ?>
 </body>
 </html>
